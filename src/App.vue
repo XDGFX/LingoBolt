89a8c21200385272846679e8df2b37a95ade83b6
XDGFX <callum.morrison@mac.com>
2022-12-25 00:18:34 +0000

    Scaffold the frontend project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <!-- Add your HTML here -->
+  </div>
+</template>
+
+<script>
+export default {
+  // Add your component logic here
+}
+</script>
+
+<style>
+/* Add your CSS here */
+</style>

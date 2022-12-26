13ef60c5fd0e333c3fd8d5282b23d6c582a19d63
XDGFX <callum.morrison@mac.com>
2022-12-26 13:22:50 +0000

    Switch to virtual scroller for performance

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -2,22 +2,39 @@
     <div
         class="m-4 h-full w-full lg:w-[990px] overflow-auto rounded-[24px] bg-white border-2 border-slate-200"
     >
-        <TransitionGroup appear name="slide-fade">
+        <!-- <TransitionGroup appear name="slide-fade">
             <card v-for="word in words" :key="word.word" :word="word"></card>
-        </TransitionGroup>
-
-        <div v-if="words.length === 0" class="text-4xl text-center p-4">
-            No results found
+        </TransitionGroup> -->
+        <div
+            v-if="words.length === 0"
+            class="text-4xl p-4 h-full flex justify-center items-center"
+        >
+            <p class="pb-4">No results found :(</p>
         </div>
+
+        <RecycleScroller
+            v-else
+            class="h-full w-full"
+            :items="words"
+            :item-size="384 + 8"
+            :key-field="'word'"
+        >
+            <template #default="{ item }">
+                <card :word="item" :showTTS="showTTS"></card>
+            </template>
+        </RecycleScroller>
     </div>
 </template>
 
 <script>
+import { RecycleScroller } from "vue-virtual-scroller";
+import "vue-virtual-scroller/dist/vue-virtual-scroller.css";
+
 import Card from "@/components/Card.vue";
 
 export default {
     name: "Carousel",
-    components: { Card },
+    components: { Card, RecycleScroller },
     props: {
         words: {
             type: Array,

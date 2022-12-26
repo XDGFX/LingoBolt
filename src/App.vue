70f66f4e3edab63365f61c99a507e90fb89cc72d
XDGFX <callum.morrison@mac.com>
2022-12-26 14:22:20 +0000

    Start work on mobile reactivity

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,44 @@
         id="app"
         class="flex flex-col items-center bg-white h-screen bg-slate-100 text-slate-900 px-4 py-8 selection:bg-rose-500 selection:text-white"
     >
-        <!-- Heading -->
-        <h1 class="text-4xl pt-8 pb-4">
-            <span class="text-slate-500">When learning a new language, </span>
-            <span class="text-slate-900 underline decoration-rose-500"
-                >the first 1000</span
-            >
-            <span class="text-slate-500"> words are the most important.</span>
-        </h1>
+        <!-- Heading desktop -->
+        <div class="collapse md:visible text-center">
+            <h1 class="text-4xl pt-8 pb-4">
+                <span class="text-slate-500">
+                    When learning a new language,
+                </span>
+                <span class="text-slate-900 underline decoration-rose-500">
+                    the first 1000
+                </span>
+                <span class="text-slate-500">
+                    words are the most important.
+                </span>
+            </h1>
 
-        <!-- Subtitle -->
-        <h2 class="text-2xl pb-12">
-            <span class="text-slate-400">
-                Here you can learn your first 1000 words in French 🇫🇷.
-            </span>
-        </h2>
+            <!-- Subtitle -->
+            <h2 class="text-2xl pb-12">
+                <span class="text-slate-400">
+                    Here you can learn your first 1000 words in French 🇫🇷.
+                </span>
+            </h2>
+        </div>
+
+        <!-- Heading mobile -->
+        <div class="visible md:collapse">
+            <h1 class="text-4xl pb-4">
+                <span class="text-slate-900 underline decoration-rose-500">
+                    the first 1000
+                </span>
+            </h1>
+        </div>
 
         <!-- Search box -->
-        <div class="flex flex-row items-center justify-center">
-            <input
-                v-model="search"
-                class="w-96 h-12 rounded-[24px] border-2 border-slate-200 p-4 text-2xl focus:border-rose-500 outline-none"
-                type="text"
-                placeholder="Search for a word"
-            />
-        </div>
+        <input
+            v-model="search"
+            class="h-8 md:h-12 w-full md:w-96 rounded-full border-2 border-slate-200 p-4 text-xl md:text-2xl focus:border-rose-500 outline-none"
+            type="text"
+            placeholder="Search for a word"
+        />
 
         <carousel :words="words"></carousel>
     </div>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,83 +1,89 @@
 <template>
-    <div
-        class="h-96 bg-slate-200 rounded-[18px] grid grid-cols-4 grid-rows-2 m-2"
-    >
+    <div class="h-96 bg-slate-200 rounded-[18px] grid md:grid-cols-2 m-2">
         <!-- word -->
         <div
-            class="row-span-2 col-span-2 flex flex-col items-center justify-center bg-slate-100 rounded-[18px] m-2"
+            class="flex flex-col items-center justify-center bg-slate-100 rounded-[18px] m-2"
         >
-            <div class="text-slate-900 whitespace-nowrap text-8xl">
+            <div
+                ref="word"
+                class="flex text-slate-900 whitespace-nowrap -mb-4"
+                :style="wordSizeStyle"
+            >
                 {{ word.word }}
             </div>
-            <div class="text-4xl text-slate-400">
+            <div class="flex md:text-4xl text-slate-400 text-center px-4 py-2">
                 {{ word.translation }}
             </div>
             <AudioPlayer v-if="showTTS" :word="word.word"></AudioPlayer>
         </div>
 
-        <!-- Example -->
-        <div class="col-span-2 flex flex-col justify-center text-2xl p-4">
-            <div class="flex justify-left items-center">
-                <div class="text-4xl pr-4">
-                    {{ word.emoji }}
-                </div>
-                <div class="col-span-2 flex flex-col justify-center text-2xl">
-                    <AudioPlayer
-                        v-if="showTTS"
-                        :word="word.example"
-                    ></AudioPlayer>
-                    <p class="pb-2">"{{ word.example }}"</p>
-                    <p class="text-slate-500">"{{ word.example_en }}"</p>
-                </div>
-            </div>
-        </div>
-
-        <!-- Tags, difficulty -->
-        <div class="col-span-2 grid p-4 content-end">
-            <div class="pb-2">
-                <div class="px-2 text-sm text-slate-900">Tags</div>
-                <div class="flex text-slate-500 text-l">
-                    <div
-                        v-for="tag in word.tags"
-                        :key="tag"
-                        class="bg-slate-300 rounded-full m-2 px-2"
-                    >
-                        {{ tag }}
+        <div class="flex flex-col">
+            <!-- Example -->
+            <div class="flex flex-grow flex-col p-4 text-l md:text-2xl">
+                <div class="flex justify-left items-center">
+                    <div class="text-4xl pr-4">
+                        {{ word.emoji }}
+                    </div>
+                    <div class="flex flex-col justify-center">
+                        <AudioPlayer
+                            v-if="showTTS"
+                            :word="word.example"
+                        ></AudioPlayer>
+                        <p class="pb-2">"{{ word.example }}"</p>
+                        <p class="text-slate-500">"{{ word.example_en }}"</p>
                     </div>
                 </div>
             </div>
 
-            <div class="pb-2">
-                <div class="grid grid-cols-2 px-2">
-                    <div class="text-sm text-slate-900">Synonyms</div>
-                    <div class="text-right text-sm text-slate-900">
-                        Antonyms
-                    </div>
-                </div>
-                <div class="flex">
-                    <div class="flex flex-1 text-slate-900 text-l">
+            <!-- Tags, difficulty -->
+            <div class="p-4">
+                <div class="pb-2 overflow-x-auto whitespace-nowrap">
+                    <div class="px-2 text-sm text-slate-900">Tags</div>
+                    <div class="flex text-slate-500 text-l">
                         <div
-                            v-for="synonym in word.synonyms"
-                            :key="synonym"
-                            class="bg-emerald-300 rounded-full m-2 px-2"
+                            v-for="tag in word.tags"
+                            :key="tag"
+                            class="bg-slate-300 rounded-full m-2 px-2"
                         >
-                            {{ synonym }}
-                        </div>
-                    </div>
-
-                    <div class="flex text-slate-900 text-l justify-self-end">
-                        <div
-                            v-for="antonym in word.antonyms"
-                            :key="antonym"
-                            class="bg-rose-300 rounded-full m-2 px-2"
-                        >
-                            {{ antonym }}
+                            {{ tag }}
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <DifficultyRange :value="word.difficulty" />
+                <div class="pb-2 overflow-x-auto whitespace-nowrap">
+                    <div class="grid grid-cols-2 px-2">
+                        <div class="text-sm text-slate-900">Synonyms</div>
+                        <div class="text-right text-sm text-slate-900">
+                            Antonyms
+                        </div>
+                    </div>
+                    <div class="flex">
+                        <div class="flex flex-1 text-slate-900 text-l">
+                            <div
+                                v-for="synonym in word.synonyms"
+                                :key="synonym"
+                                class="bg-emerald-300 rounded-full m-2 px-2"
+                            >
+                                {{ synonym }}
+                            </div>
+                        </div>
+
+                        <div
+                            class="flex text-slate-900 text-l justify-self-end"
+                        >
+                            <div
+                                v-for="antonym in word.antonyms"
+                                :key="antonym"
+                                class="bg-rose-300 rounded-full m-2 px-2"
+                            >
+                                {{ antonym }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <DifficultyRange :value="word.difficulty" />
+            </div>
         </div>
     </div>
 </template>
@@ -102,7 +108,32 @@ export default {
             default: false,
         },
     },
+    computed: {
+        wordSizeStyle() {
+            // Based on the number of characters in the word, we can estimate the font size
+            // <8 characters: 96px
+            // 8-12 characters: 72px
+            // 12-16 characters: 48px
+            // 16-20 characters: 36px
+            // 20+ characters: 24px
+
+            const wordLength = this.word.word.length;
+            let fontSize = 96;
+
+            if (wordLength >= 8 && wordLength < 12) {
+                fontSize = 72;
+            } else if (wordLength >= 12 && wordLength < 16) {
+                fontSize = 48;
+            } else if (wordLength >= 16 && wordLength < 20) {
+                fontSize = 36;
+            } else if (wordLength >= 20) {
+                fontSize = 24;
+            }
+
+            return {
+                fontSize: `${fontSize}px`,
+            };
+        },
+    },
 };
 </script>
-
-<style scoped></style>

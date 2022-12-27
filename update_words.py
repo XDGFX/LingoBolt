001a74c527f6569d0962c26ac674343cb510e01b
XDGFX <callum.morrison@mac.com>
2022-12-27 14:38:21 +0000

    Update prompt and code to improve batch processing

--- update_words.py
+++ update_words.py
@@ -10,22 +10,11 @@ OUTPUT_FILE = os.path.join("src", "french.json")
 BATCH = 3
 
 
-def get_word(word: str) -> typing.Union[None, dict]:
-    response = input(f"{word}:\n").strip().strip(",").strip()
-
-    # The response should be a valid JSON object, in the format of:
-    # {"word":"<word>","translation":"<translation>","example":"<example>","example_en":"<example_en>","emoji":"<emoji>","tags":"[<tags>]","difficulty":"<difficulty>","part_of_speech":"<part_of_speech>","gender":<gender>,"plural":<plural>,"synonyms":[<synonyms>],"antonyms":[<antonyms>]}
-    try:
-        input_obj = json.loads(response)
-
-    except json.JSONDecodeError:
-        print("Invalid JSON string")
-        return None
-
+def validate_word(word: str, input_obj: dict) -> bool:
     # Check that the input object matches the required format
     if not isinstance(input_obj, dict):
         print("Input must be a JSON object")
-        return None
+        return False
 
     required_keys = [
         "word",
@@ -44,18 +33,51 @@ def get_word(word: str) -> typing.Union[None, dict]:
     for key in required_keys:
         if key not in input_obj:
             print(f"Missing required key: {key}")
-            return None
+            return False
 
     # Make sure the word is the same as the one we are adding
     if input_obj["word"] != word:
         print("Word does not match")
+        return False
+
+    # Make sure the tags are a list
+    if not isinstance(input_obj["tags"], list):
+        print("Tags must be a list")
+        return False
+
+    # Make sure the synonyms and antonyms are lists
+    if not isinstance(input_obj["synonyms"], list):
+        print("Synonyms must be a list")
+        return False
+
+    if not isinstance(input_obj["antonyms"], list):
+        print("Antonyms must be a list")
+        return False
+
+    # Make sure the difficulty is a number
+    if not isinstance(input_obj["difficulty"], int):
+        print("Difficulty must be a number")
+        return False
+
+    return True
+
+
+def get_response(word: str) -> typing.Union[None, dict]:
+    response = input(f"{word}\n").strip().strip(",").strip()
+
+    # The response should be a valid JSON object, in the format of:
+    # {"word":"<word>","translation":"<translation>","example":"<example>","example_en":"<example_en>","emoji":"<emoji>","tags":"[<tags>]","difficulty":"<difficulty>","part_of_speech":"<part_of_speech>","gender":<gender>,"plural":<plural>,"synonyms":[<synonyms>],"antonyms":[<antonyms>]}
+    try:
+        input_obj = json.loads("[" + response + "]")
+
+    except json.JSONDecodeError:
+        print("Invalid JSON string")
         return None
 
     return input_obj
 
 
 def main():
-
     # First we load all the words from WORD_FILE into a list
     with open(WORD_FILE, "r") as f:
         words = json.load(f)
@@ -73,24 +95,33 @@ def main():
 
     # We now iterate over every BATCH words and display them to the user.
     for i in range(0, len(words), BATCH):
+        print("\n")
 
         start_time = time.time()
 
         # Get the next BATCH words
         next_words = words[i : i + BATCH]
 
-        for word in next_words:
-            print(word, end=", ")
+        next_words_string = ", ".join(next_words)
 
-        print("\n")
+        # Get the response from the user
+        input_obj = get_response(next_words_string)
 
+        # If the response is invalid, we skip this word
+        if not input_obj:
+            continue
+
+        # If the response is valid, we validate it
         for word in next_words:
-            obj = get_word(word)
-            if obj is None:
+            # Find the object for this word
+            word_obj = [x for x in input_obj if x["word"] == word][0]
+
+            # Validate the object
+            if not validate_word(word, word_obj):
                 continue
 
             # Add the word to the existing words
-            existing_words.append(obj)
+            existing_words.append(word_obj)
 
             # Update the output file
             with open(OUTPUT_FILE, "w") as f:

e25481de0235f0423b954608a99511ef49e51488
XDGFX <callum.morrison@mac.com>
2022-12-27 21:31:53 +0000

    Finish elo bar

--- src/components/EloRange.vue
+++ src/components/EloRange.vue
@@ -1,25 +1,38 @@
 <template>
-    <div class="bg-slate-200 rounded-full m-2 p-2">
-        <div class="relative rounded-full">
-            <div
-                class="flex w-full rounded-md leading-5 text-slate-500 focus:outline-none focus:shadow-outline-blue-300 transition duration-150 ease-in-out"
-            >
+    <div>
+        <div class="bg-slate-200 rounded-full m-2 p-2">
+            <div class="relative rounded-full">
                 <div
-                    class="h-4 bg-cyan-400 rounded-full transition-all"
-                    :style="{
-                        width: `${
-                            ((elo + 1 - minMax.min) /
-                                (minMax.max - minMax.min)) *
-                            100
-                        }%`,
-                    }"
-                ></div>
+                    class="flex w-full rounded-md leading-5 text-slate-500 focus:outline-none focus:shadow-outline-blue-300 transition duration-150 ease-in-out"
+                >
+                    <div
+                        class="h-4 bg-cyan-400 rounded-full motion-safe:transition-all"
+                        :style="{
+                            width: `${
+                                ((elo + 1 - minMax.min) /
+                                    (minMax.max - minMax.min)) *
+                                100
+                            }%`,
+                        }"
+                    ></div>
+                </div>
+            </div>
+        </div>
+        <div class="flex mx-2">
+            <div class="flex-1 text-start text-slate-500">
+                Level {{ currentLevel }}
+            </div>
+            <div class="flex-1 text-center text-slate-500">Elo: {{ elo }}</div>
+            <div class="flex-1 text-end text-slate-500">
+                Level {{ currentLevel + 1 }}
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import JSConfetti from "js-confetti";
+
 export default {
     name: "DifficultyRange",
     data() {
@@ -39,6 +52,7 @@ export default {
 
         return {
             levels,
+            confetti: new JSConfetti(),
         };
     },
     props: {
@@ -57,6 +71,22 @@ export default {
         },
         currentLevel() {
             return this.levels.findIndex((level) => level > this.elo);
+        },
+    },
+    watch: {
+        currentLevel: {
+            handler(newValue, oldValue) {
+                const prefersReducedMotion = window.matchMedia(
+                    "(prefers-reduced-motion: reduce)"
+                );
+
+                if (prefersReducedMotion.matches) {
+                    return;
+                } else if (newValue > oldValue) {
+                    this.confetti.addConfetti();
+                }
+            },
+            immediate: true,
         },
     },
 };

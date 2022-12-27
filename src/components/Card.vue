29d8043808c6c0eb7d3933cea0f9472c641f6b85
XDGFX <callum.morrison@mac.com>
2022-12-27 16:50:21 +0000

    Add additional information

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="h-96 bg-slate-200 rounded-[18px] grid md:grid-cols-2 m-2">
+    <div
+        class="min-h-[384px] bg-slate-200 rounded-[18px] grid md:grid-cols-2 m-2"
+    >
         <!-- word -->
         <div
             class="flex flex-col items-center justify-center bg-slate-100 rounded-[18px] m-2"
         >
             <div
                 ref="word"
-                class="flex text-slate-900 whitespace-nowrap -mb-4 md:mb-0"
+                class="flex text-slate-900 whitespace-nowrap -my-4 md:mb-0"
                 :style="wordSizeStyle"
             >
                 {{ word.word }}
@@ -17,22 +19,28 @@
             >
                 {{ word.translation }}
             </div>
-            <AudioPlayer v-if="showTTS" :word="word.word"></AudioPlayer>
+            <AudioPlayer
+                class="pb-2"
+                v-if="showTTS"
+                :word="word.word"
+            ></AudioPlayer>
         </div>
 
-        <div class="flex flex-col">
+        <div class="flex flex-col p-4">
             <!-- Example -->
-            <div class="flex flex-grow flex-col p-4 text-l md:text-2xl">
+            <div class="flex flex-grow flex-col text-l md:text-2xl">
                 <div class="flex justify-left items-center">
                     <div class="text-4xl pr-4">
                         {{ word.emoji }}
                     </div>
                     <div class="flex flex-col justify-center gap-2">
-                        <AudioPlayer
-                            v-if="showTTS"
-                            :word="word.example"
-                        ></AudioPlayer>
-                        <p>"{{ word.example }}"</p>
+                        <div class="flex gap-2 align-middle">
+                            <AudioPlayer
+                                v-if="showTTS"
+                                :word="word.example"
+                            ></AudioPlayer>
+                            <p>"{{ word.example }}"</p>
+                        </div>
                         <p class="text-slate-500" v-show="!hideTranslations">
                             "{{ word.example_en }}"
                         </p>
@@ -40,55 +48,96 @@
                 </div>
             </div>
 
-            <!-- Tags, difficulty -->
-            <div class="p-4">
-                <div class="pb-2 overflow-x-auto whitespace-nowrap">
-                    <div class="px-2 text-sm text-slate-900">Tags</div>
+            <!-- Tags -->
+            <!-- <div class="pb-2 overflow-x-auto whitespace-nowrap">
+                <div class="px-2 text-sm text-slate-900">Tags</div>
+                <div class="flex text-slate-500 text-l">
+                    <div
+                        v-for="tag in word.tags"
+                        :key="tag"
+                        class="bg-slate-300 rounded-full m-2 px-2"
+                    >
+                        {{ tag }}
+                    </div>
+                </div>
+            </div> -->
+
+            <!-- Part of speech -->
+            <div class="flex justify-between overflow-x-auto whitespace-nowrap">
+                <div
+                    v-if="word.part_of_speech"
+                    class="pb-2 overflow-x-auto whitespace-nowrap"
+                >
+                    <div class="px-2 text-sm text-slate-900">
+                        Part of Speech
+                    </div>
                     <div class="flex text-slate-500 text-l">
-                        <div
-                            v-for="tag in word.tags"
-                            :key="tag"
-                            class="bg-slate-300 rounded-full m-2 px-2"
-                        >
-                            {{ tag }}
+                        <div class="bg-slate-300 rounded-full m-2 px-2">
+                            {{ word.part_of_speech }}
                         </div>
                     </div>
                 </div>
 
-                <div class="pb-2">
-                    <div class="grid grid-cols-2 px-2">
-                        <div class="text-sm text-slate-900">Synonyms</div>
-                        <div class="text-right text-sm text-slate-900">
-                            Antonyms
-                        </div>
-                    </div>
-                    <div class="flex overflow-x-auto whitespace-nowrap">
-                        <div class="flex flex-1 text-slate-900 text-l pr-4">
-                            <div
-                                v-for="synonym in word.synonyms"
-                                :key="synonym"
-                                class="bg-emerald-300 rounded-full m-2 px-2"
-                            >
-                                {{ synonym }}
-                            </div>
-                        </div>
-
-                        <div
-                            class="flex text-slate-900 text-l justify-self-end"
-                        >
-                            <div
-                                v-for="antonym in word.antonyms"
-                                :key="antonym"
-                                class="bg-rose-300 rounded-full m-2 px-2"
-                            >
-                                {{ antonym }}
-                            </div>
+                <!-- Gender -->
+                <div
+                    v-if="word.gender"
+                    class="pb-2 overflow-x-auto whitespace-nowrap"
+                >
+                    <div class="px-2 text-sm text-slate-900">Gender</div>
+                    <div class="flex text-slate-500 text-l">
+                        <div class="bg-slate-300 rounded-full m-2 px-2">
+                            {{ word.gender }}
                         </div>
                     </div>
                 </div>
 
-                <DifficultyRange :value="word.difficulty" />
+                <!-- Plural -->
+                <div
+                    v-if="word.plural"
+                    class="pb-2 overflow-x-auto whitespace-nowrap"
+                >
+                    <div class="px-2 text-sm text-slate-900">Plural</div>
+                    <div class="flex text-slate-500 text-l">
+                        <div class="bg-slate-300 rounded-full m-2 px-2">
+                            {{ word.plural }}
+                        </div>
+                    </div>
+                </div>
             </div>
+
+            <!-- Synonyms and Antonyms -->
+            <div v-if="word.synonyms.length > 0 || word.antonyms.length > 0">
+                <div class="grid grid-cols-2 px-2">
+                    <div class="text-sm text-slate-900">Synonyms</div>
+                    <div class="text-right text-sm text-slate-900">
+                        Antonyms
+                    </div>
+                </div>
+                <div class="flex overflow-x-auto whitespace-nowrap">
+                    <div class="flex flex-1 text-slate-900 text-l pr-4">
+                        <div
+                            v-for="synonym in word.synonyms"
+                            :key="synonym"
+                            class="bg-emerald-300 rounded-full m-2 px-2"
+                        >
+                            {{ synonym }}
+                        </div>
+                    </div>
+
+                    <div class="flex text-slate-900 text-l justify-self-end">
+                        <div
+                            v-for="antonym in word.antonyms"
+                            :key="antonym"
+                            class="bg-rose-300 rounded-full m-2 px-2"
+                        >
+                            {{ antonym }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Difficulty Range -->
+            <DifficultyRange :value="word.difficulty" />
         </div>
     </div>
 </template>

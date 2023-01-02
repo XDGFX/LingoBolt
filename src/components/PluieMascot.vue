a2ed2520964dabdaf636da850d2eabfbce959747
XDGFX <callum.morrison@mac.com>
2023-01-02 14:59:41 +0000

    Add Pluie speech support

--- src/components/PluieMascot.vue
+++ src/components/PluieMascot.vue
@@ -1,56 +1,78 @@
 <template>
-    <div class="w-48 h-48 md:w-64 md:h-64 pointer-events-none select-none">
-        <img
-            class="absolute w-48 h-48 md:w-64 md:h-64 floating delayed z-10"
-            src="@/assets/pluie/bobble.svg"
-        />
-        <div class="absolute w-48 h-48 md:w-64 md:h-64 floating">
+    <div
+        class="flex items-center justify-center"
+        :class="{
+            'flex-wrap-reverse w-min': speechPosition === 'top',
+            'flex-nowrap': speechPosition === 'right',
+        }"
+    >
+        <div class="w-48 h-48 md:w-64 md:h-64 pointer-events-none select-none">
             <img
-                ref="base"
-                class="absolute z-10"
-                src="@/assets/pluie/base.svg"
-                alt="Pluie mascot"
+                class="absolute w-48 h-48 md:w-64 md:h-64 floating delayed z-10"
+                src="@/assets/pluie/bobble.svg"
             />
+            <div class="absolute w-48 h-48 md:w-64 md:h-64 floating">
+                <img
+                    ref="base"
+                    class="absolute z-10"
+                    src="@/assets/pluie/base.svg"
+                    alt="Pluie mascot"
+                />
 
-            <!-- Face -->
-            <img
-                v-if="mood === 'smile'"
-                class="absolute z-30"
-                src="@/assets/pluie/face_smile.svg"
-            />
-            <img
-                v-else-if="mood === 'grin'"
-                class="absolute z-30"
-                src="@/assets/pluie/face_grin.svg"
-            />
-            <img
-                v-else-if="mood === 'interest'"
-                class="absolute z-30"
-                src="@/assets/pluie/face_interest.svg"
-            />
-            <img
-                v-else-if="mood === 'content'"
-                class="absolute z-30"
-                src="@/assets/pluie/face_content.svg"
-            />
+                <!-- Face -->
+                <img
+                    v-if="mood === 'smile'"
+                    class="absolute z-30"
+                    src="@/assets/pluie/face_smile.svg"
+                />
+                <img
+                    v-else-if="mood === 'grin'"
+                    class="absolute z-30"
+                    src="@/assets/pluie/face_grin.svg"
+                />
+                <img
+                    v-else-if="mood === 'interest'"
+                    class="absolute z-30"
+                    src="@/assets/pluie/face_interest.svg"
+                />
+                <img
+                    v-else-if="mood === 'content'"
+                    class="absolute z-30"
+                    src="@/assets/pluie/face_content.svg"
+                />
 
-            <!-- Accessories -->
-            <img
-                v-if="accessory === 'hat'"
-                class="absolute z-40"
-                src="@/assets/pluie/accessory_hat.svg"
-            />
-            <img
-                v-else-if="accessory === 'glasses'"
-                class="absolute z-40"
-                src="@/assets/pluie/accessory_glasses.svg"
-            />
+                <!-- Accessories -->
+                <img
+                    v-if="accessory === 'hat'"
+                    class="absolute z-40"
+                    src="@/assets/pluie/accessory_hat.svg"
+                />
+                <img
+                    v-else-if="accessory === 'glasses'"
+                    class="absolute z-40"
+                    src="@/assets/pluie/accessory_glasses.svg"
+                />
 
-            <img
-                v-if="speaking"
-                class="absolute z-10"
-                src="@/assets/pluie/speech.svg"
-            />
+                <img
+                    v-if="speaking"
+                    class="absolute z-10"
+                    src="@/assets/pluie/speech.svg"
+                />
+            </div>
+        </div>
+        <div
+            v-if="speech"
+            class="relative text-center m-4 p-2 max-w-[12em] min-w-[4em] rounded-[24px] bg-white border-2 border-slate-300"
+        >
+            <p>{{ speech }}</p>
+            <div
+                v-if="speechPosition === 'top'"
+                class="absolute left-1/2 bottom-0 translate-y-[7.5px] -translate-x-1/2 -rotate-45 h-3 w-3 rounded-bl bg-white border-l-2 border-b-2 border-slate-300"
+            ></div>
+            <div
+                v-if="speechPosition === 'right'"
+                class="absolute bottom-1/2 left-0 -translate-x-[6.5px] translate-y-1/2 rotate-45 h-3 w-3 rounded-bl bg-white border-l-2 border-b-2 border-slate-300"
+            ></div>
         </div>
     </div>
 </template>
@@ -70,9 +92,14 @@ export default {
             default: "",
             validator: (value) => ["", "hat", "glasses"].includes(value),
         },
-        speaking: {
-            type: Boolean,
-            default: false,
+        speech: {
+            type: String,
+            default: "",
+        },
+        speechPosition: {
+            type: String,
+            default: "right",
+            validator: (value) => ["right", "top"].includes(value),
         },
     },
 };

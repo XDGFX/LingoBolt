f80a12fe2a3c4871a4023bad9adfed7693f23a33
XDGFX <callum.morrison@mac.com>
2023-01-06 19:56:46 +0000

    Possible next version

--- scripts/full_auto_update.py
+++ scripts/full_auto_update.py
@@ -14,30 +14,35 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-PROMPT_TEMPLATE = """I am going to send you some words in ${language_english}. I want you to respond exactly with the following message for each word:
-"{"word":"<word>","translation":"<translation>","example":"<example>","example_en":"<example_en>","emoji":"<emoji>","tags":[<tags>],"difficulty":<difficulty>,"part_of_speech":"<part_of_speech>","gender":"<gender>","plural":"<plural>","synonyms":[<synonyms>],"antonyms":[<antonyms>]},"
+PROMPT_TEMPLATE = """
+I am going to send you some words in ${language_english}. I want you to respond in JSON
+format for each word, with the following fields for each entry:
 
-Where <word> is the word in ${language_english}
-<translation> is the translation of the word in English
-<example> is an example sentence in ${language_english}
-<example_en> is the English translation of the example sentence
-<emoji> is an emoji that could be associated with the word
-<tags> is a comma separated list of tags (categorical groupings for the word)
-<difficulty> is the difficulty of the word from 1-4, with 1 being the easiest
-<part_of_speech> is where the word could be used in speech (noun, verb, adjectives, adverbs, etc.)
-<gender> is the gender of the word (if applicable)
-<plural> is the plural form of the word (if applicable)
-<synonyms> is a comma separated list of synonyms (similar words that mean the same thing)
-<antonyms> is a comma separated list of antonyms (words that mean the opposite of the word)
+word: foreign word
+tran: translation of the word in English
+ex: example sentence in the foreign language
+ex_en: English translation of the example sentence
+em:  emoji that could be associated with the word
+tags: list of <4 tags (categorical groupings for the word)
+diff: the difficulty of the word from 1-4, with 1 being the easiest
+part: where the word could be used in speech (noun, verb, adjectives, adverbs, etc.)
+gen: gender of the word m/f/n (if applicable)
+pl: plural form of the word (if applicable)
+sy: list of <4 synonyms
+an: list of <4 antonyms
+pro: pronunciation of the word (broad transcription)
+con: list of conjugations of the word (if applicable)
+der: list of <4 derivatives of the word (if applicable)
+col: list of <4 collocations of the word (if applicable)
 
 The example sentence should be short, varied, and interesting in each case.
+${non_latin_prompt}
+The response should contain only a one-line JSON array, and nothing else
 
-For example, if the prompt  was "${example_input}", the response could be exactly:
-${example_output}
-The response should contain only one-line of JSON, and nothing else
+e.g. object for 'jardin' in French:
+{"word": "jardin", "tran": "garden", "ex": "Mon jardin est très grand.", "ex_en": "My garden is very large.", "em": "🌺", "tags": ["nature", "outdoors"], "diff": 2, "part": "noun", "gen": "m", "pl": "jardins", "sy": ["parc", "terrasse"], "an": ["appartement"], "pro": "zhahr-dehn", "con": null, "der": ["jardinier", "jardinière"], "col": ["un jardin potager", "un jardin d'enfants"]}
 
 Here are the first ${batch_size} words:
-
 """
 
 
@@ -49,17 +54,21 @@ def validate_word(word: str, input_obj: dict) -> bool:
 
     required_keys = [
         "word",
-        "translation",
-        "example",
-        "example_en",
-        "emoji",
+        "tran",
+        "ex",
+        "ex_en",
+        "em",
         "tags",
-        "difficulty",
-        "part_of_speech",
-        "gender",
-        "plural",
-        "synonyms",
-        "antonyms",
+        "diff",
+        "part",
+        "gen",
+        "pl",
+        "sy",
+        "an",
+        "pro",
+        "con",
+        "der",
+        "col",
     ]
     for key in required_keys:
         if key not in input_obj:
@@ -77,16 +86,29 @@ def validate_word(word: str, input_obj: dict) -> bool:
         return False
 
     # Make sure the synonyms and antonyms are lists
-    if not isinstance(input_obj["synonyms"], list):
+    if not isinstance(input_obj["sy"], list):
         print("Synonyms must be a list")
         return False
 
-    if not isinstance(input_obj["antonyms"], list):
+    if not isinstance(input_obj["an"], list):
         print("Antonyms must be a list")
         return False
 
+    # Make sure derivatives, collocations, and conjugations are lists
+    if not isinstance(input_obj["der"], list):
+        print("Derivatives must be a list")
+        return False
+
+    if not isinstance(input_obj["col"], list):
+        print("Collocations must be a list")
+        return False
+
+    if not isinstance(input_obj["con"], list):
+        print("Conjugations must be a list")
+        return False
+
     # Make sure the difficulty is a number
-    if not isinstance(input_obj["difficulty"], int):
+    if not isinstance(input_obj["diff"], int):
         print("Difficulty must be a number")
         return False
 
@@ -95,28 +117,39 @@ def validate_word(word: str, input_obj: dict) -> bool:
 
 def get_response(prompt: str) -> str:
     # Get a response from the API
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=3297,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-    )
+    try:
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=3297,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+    except:
+        print("Error with OpenAI API")
+        return None
 
     response_text = response["choices"][0]["text"]
 
     response = response_text.strip().strip(",").strip()
 
+    # Add square brackets to the response if they are missing
+    if response[0] != "[":
+        response = "[" + response
+
+    if response[-1] != "]":
+        response = response + "]"
+
     try:
-        input_obj = json.loads("[" + response + "]")
+        input_obj = json.loads(response)
 
     except json.JSONDecodeError as e:
         print("Invalid JSON string")
         print()
         print(response)
-        raise e
+        return None
 
     return input_obj
 
@@ -154,6 +187,14 @@ def setup():
         "--example_output",
         type=str,
         help="The example output for the prompt",
+    )
+
+    # Boolean flag to indicate the language uses a non Latin script
+    parser.add_argument(
+        "-n",
+        "--non_latin",
+        action="store_true",
+        help="Whether the language uses a non Latin script",
     )
 
     args = parser.parse_args()
@@ -249,42 +290,57 @@ def setup():
         )["language_english"]
     )
 
-    # Then we ask for an example input (comma separated list of 2-3 words in the
-    # language)
-    example_input = (
-        args.example_input
-        or inquirer.prompt(
-            [
-                inquirer.Text(
-                    "example_input",
-                    message="What is an example input? (comma separated list of 2-3 words in the language)",
-                    default="algunos,abajo",
-                )
-            ]
-        )["example_input"]
-    )
+    # # Then we ask for an example input (comma separated list of 2-3 words in the
+    # # language)
+    # example_input = (
+    #     args.example_input
+    #     or inquirer.prompt(
+    #         [
+    #             inquirer.Text(
+    #                 "example_input",
+    #                 message="What is an example input? (comma separated list of 2-3 words in the language)",
+    #                 default="algunos,abajo",
+    #             )
+    #         ]
+    #     )["example_input"]
+    # )
 
-    # Then we ask for an example output (the JSON response for the example
-    # input)
-    example_output = (
-        args.example_output
+    # # Then we ask for an example output (the JSON response for the example
+    # # input)
+    # example_output = (
+    #     args.example_output
+    #     or inquirer.prompt(
+    #         [
+    #             inquirer.Editor(
+    #                 "example_output",
+    #                 message="What is an example output? (the JSON response for the example input)",
+    #                 default='{{"word":"alguno","translation":"some","example":"Algunos de ellos llegaron tarde","example_en":"Some of them arrived late","emoji":"🕰️","tags":["quantifier"],"difficulty":1,"part_of_speech":"determiner","gender":"male","plural":"algunos","synonyms":["ciertos","unos cuantos"],"antonyms":[]}},{{"word":"abajo","translation":"down","example":"Corre abajo para llegar a tiempo","example_en":"Run down to get there on time","emoji":"🏃‍♀️","tags":["direction", "movement"],"difficulty":1,"part_of_speech":"adverb","gender":null,"plural":null,"synonyms":["bajo","debajo"],"antonyms":["arriba"]}},',
+    #             )
+    #         ]
+    #     )["example_output"]
+    # )
+
+    # Ask if the language uses a non Latin script
+    non_latin = (
+        args.non_latin
         or inquirer.prompt(
             [
-                inquirer.Editor(
-                    "example_output",
-                    message="What is an example output? (the JSON response for the example input)",
-                    default='{{"word":"alguno","translation":"some","example":"Algunos de ellos llegaron tarde","example_en":"Some of them arrived late","emoji":"🕰️","tags":["quantifier"],"difficulty":1,"part_of_speech":"determiner","gender":"male","plural":"algunos","synonyms":["ciertos","unos cuantos"],"antonyms":[]}},{{"word":"abajo","translation":"down","example":"Corre abajo para llegar a tiempo","example_en":"Run down to get there on time","emoji":"🏃‍♀️","tags":["direction", "movement"],"difficulty":1,"part_of_speech":"adverb","gender":null,"plural":null,"synonyms":["bajo","debajo"],"antonyms":["arriba"]}},',
+                inquirer.Confirm(
+                    "non_latin",
+                    message="Does the language use a non Latin script?",
+                    default=False,
                 )
             ]
-        )["example_output"]
+        )["non_latin"]
     )
 
     # We can now format the prompt
     template = Template(PROMPT_TEMPLATE)
     prompt = template.substitute(
         language_english=language_english,
-        example_input=example_input,
-        example_output=example_output,
+        non_latin_prompt="Include a transliteration in square brackets\n"
+        if non_latin
+        else "",
         batch_size=batch_size,
     )
 
@@ -338,11 +394,14 @@ def main():
         next_words_string = ",".join(next_words)
 
         # Get the response from the API
+        response_retries = 0
         input_obj = get_response(setup_results["prompt"] + next_words_string + "\n")
 
-        # If the response is invalid, we raise an error
-        if not input_obj:
-            raise Exception("Invalid response")
+        # If the response is invalid, we retry up to 3 times
+        while not input_obj and response_retries < 3:
+            print("Invalid response, retrying...")
+            response_retries += 1
+            input_obj = get_response(setup_results["prompt"] + next_words_string + "\n")
 
         # If the response is valid, we validate it
         for word in next_words:

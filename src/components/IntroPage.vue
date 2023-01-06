7f91c05ce14d630497a0a299c7f694f2f2c81c4b
XDGFX <callum.morrison@mac.com>
2023-01-06 23:40:09 +0000

    Add disclaimer

--- src/components/IntroPage.vue
+++ src/components/IntroPage.vue
@@ -1,6 +1,9 @@
 <template>
     <!-- Div that covers the whole screen -->
-    <div v-if="show" class="fixed inset-0 overflow-y-auto bg-slate-200 z-50">
+    <div
+        v-if="show"
+        class="fixed flex inset-0 overflow-y-auto bg-slate-200 z-50 justify-center"
+    >
         <div
             class="flex flex-col items-center justify-center text-slate-800 h-full"
         >
@@ -31,7 +34,7 @@
                 </h2>
 
                 <button
-                    class="flex items-center justify-center h-12 w-48 rounded-full border-2 border-slate-300 p-4 m-8 text-xl md:text-2xl bg-white hover:bg-slate-200 outline-none"
+                    class="flex items-center justify-center h-12 w-48 rounded-full border-2 border-slate-300 p-4 m-8 text-xl md:text-2xl bg-white hover:bg-slate-100 outline-none"
                     @click="page = 1"
                 >
                     Next
@@ -46,9 +49,7 @@
                 <h2 class="text-4xl pt-8 pb-4 text-slate-500">
                     Here are some tips:
                 </h2>
-                <ul
-                    class="text-xl md:text-2xl md:list-disc md:text-left gap-4 flex flex-col"
-                >
+                <ul class="text-xl md:text-2xl gap-4 flex flex-col">
                     <li>Revise 📖 new words on the following screen.</li>
                     <li>
                         When you are ready, go to the Practice 🎓 page to test
@@ -59,8 +60,20 @@
                         alongside other learning tools.
                     </li>
                 </ul>
+
+                <h2 class="text-2xl pt-8 pb-4 text-slate-500">Disclaimer:</h2>
+                <p class="text-s gap-4 flex flex-col lg:w-[990px]">
+                    While I strive to provide accurate and reliable
+                    translations, please keep in mind that language is
+                    constantly evolving and that my translations may not always
+                    reflect the most current usage. Use this site as a guide to
+                    help you understand new words and phrases, but be open to
+                    learning and growing as you continue your language learning
+                    journey!
+                </p>
+
                 <button
-                    class="flex self-center items-center justify-center h-12 w-48 rounded-full border-2 border-slate-300 p-4 m-8 text-xl md:text-2xl bg-white hover:bg-slate-200 outline-none"
+                    class="flex self-center items-center justify-center h-12 w-48 rounded-full border-2 border-slate-300 p-4 m-8 text-xl md:text-2xl bg-white hover:bg-slate-100 outline-none"
                     @click="show = false"
                 >
                     Let's go!

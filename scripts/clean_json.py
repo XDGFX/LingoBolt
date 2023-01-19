8de10350c607b698227783b0b3b76d85367eb23b
XDGFX <callum.morrison@mac.com>
2023-01-19 19:51:58 +0000

    Auto clean all languages

--- scripts/clean_json.py
+++ scripts/clean_json.py
@@ -2,26 +2,27 @@
 
 import json
 import os
+from tqdm import tqdm
 
-WORD_LIST = os.path.join("..", "wordlists", "fr.txt")
-JSON_FILE = os.path.join("..", "src", "languages", "fr.json")
-OUTPUT_FILE = os.path.join("..", "src", "languages", "french_new.json")
+_script_path = os.path.dirname(os.path.realpath(__file__))
+
+WORD_LIST_DIR = os.path.join(_script_path, "..", "wordlists")
+LANGUAGE_DIR = os.path.join(_script_path, "..", "src", "languages")
 
 
-def main():
-
+def clean(word_list, json_file):
     # Load all the words from the wordlist
-    with open(WORD_LIST) as f:
+    with open(word_list) as f:
         words = f.read().splitlines()
 
     print(f"Loaded {len(words)} words from wordlist.")
 
     # Load the words JSON file
-    with open(JSON_FILE) as f:
+    with open(json_file) as f:
         data = json.load(f)
 
     print(f"Loaded {len(data)} words from JSON file.")
-    print(f"Original file size: {os.path.getsize(JSON_FILE)} bytes")
+    print(f"Original file size: {os.path.getsize(json_file)} bytes")
 
     # Remove all the words that are not in the wordlist
     data = [w for w in data if w["word"] in words]
@@ -39,11 +40,25 @@ def main():
     print(f"New word count: {len(data)}")
 
     # Write the new JSON file
-    with open(OUTPUT_FILE, "w") as f:
+    with open(json_file, "w") as f:
         json.dump(data, f, ensure_ascii=False)
 
     print("Done.")
-    print(f"New file size: {os.path.getsize(OUTPUT_FILE)} bytes")
+    print(f"New file size: {os.path.getsize(json_file)} bytes")
+
+
+def main():
+    # Loop over all json files in language dir
+    for file in os.listdir(LANGUAGE_DIR):
+        if not file.endswith(".json"):
+            print(f"Skipping non json file: {file}")
+            continue
+
+        # Clean the file
+        clean(
+            os.path.join(WORD_LIST_DIR, file.replace(".json", ".txt")),
+            os.path.join(LANGUAGE_DIR, file),
+        )
 
 
 if __name__ == "__main__":
